9c5bd21ae44f81255815ce0d184c4e1319e8f415
Marc Wood <metamarcdw@gmail.com>
2015-05-31 21:09:31 -0400

    Add GUI imports to qtui/__init__.py

---
+++ qtui/__init__.py
@@ -0,0 +1,14 @@
+from PyQt4 import QtCore, QtGui
+from PyQt4.QtCore import *
+from PyQt4.QtGui import *
+
+from metamarket_qt import *
+from view_ident_dialog import *
+from view_offer_dialog import *
+from view_market_dialog import *
+from view_chanmsg_dialog import *
+from import_market_dialog import *
+from send_chanmsg_dialog import *
+from about_dialog import *
+
+
