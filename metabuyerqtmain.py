0ad0f84ccb3a142a4d44602e54ee3d3dfd9fb27e
Marc Wood <metamarcdw@gmail.com>
2015-06-12 22:01:17 -0400

    Add classes for dialogs and minimal code for main app

---
+++ metabuyerqtmain.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+class Main:
+    def start(self):
+        import qtui
+        qtui.run()
+
+if __name__ == "__main__":
+    mainprogram = Main()
+    mainprogram.start()
+
--- qtui/__init__.py
+++ qtui/__init__.py
@@ -1,3 +1,5 @@
+import sys
+
 from PyQt4 import QtCore, QtGui
 from PyQt4.QtCore import *
 from PyQt4.QtGui import *
@@ -11,4 +13,77 @@ from import_market_dialog import *
 from send_chanmsg_dialog import *
 from about_dialog import *
 
+class MyForm(QtGui.QMainWindow):
+    
+    def __init__(self, parent=None):
+        QtGui.QWidget.__init__(self, parent)
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+        
+        
+        
+    
+class viewIdentDialog(QtGui.QDialog):
+    def __init__(self, parent):
+        QtGui.QWidget.__init__(self, parent)
+        self.ui = Ui_view_ident_dialog()
+        self.ui.setupUi(self)
+        self.parent = parent
+        QtGui.QWidget.resize(self, QtGui.QWidget.sizeHint(self))
 
+class viewOfferDialog(QtGui.QDialog):
+    def __init__(self, parent):
+        QtGui.QWidget.__init__(self, parent)
+        self.ui = Ui_view_offer_dialog()
+        self.ui.setupUi(self)
+        self.parent = parent
+        QtGui.QWidget.resize(self, QtGui.QWidget.sizeHint(self))
+
+class viewMarketDialog(QtGui.QDialog):
+    def __init__(self, parent):
+        QtGui.QWidget.__init__(self, parent)
+        self.ui = Ui_view_market_dialog()
+        self.ui.setupUi(self)
+        self.parent = parent
+        QtGui.QWidget.resize(self, QtGui.QWidget.sizeHint(self))
+
+class viewChanmsgDialog(QtGui.QDialog):
+    def __init__(self, parent):
+        QtGui.QWidget.__init__(self, parent)
+        self.ui = Ui_view_chanmsg_dialog()
+        self.ui.setupUi(self)
+        self.parent = parent
+        QtGui.QWidget.resize(self, QtGui.QWidget.sizeHint(self))
+
+class importMarketDialog(QtGui.QDialog):
+    def __init__(self, parent):
+        QtGui.QWidget.__init__(self, parent)
+        self.ui = Ui_import_market_dialog()
+        self.ui.setupUi(self)
+        self.parent = parent
+        QtGui.QWidget.resize(self, QtGui.QWidget.sizeHint(self))
+
+class sendChanmsgDialog(QtGui.QDialog):
+    def __init__(self, parent):
+        QtGui.QWidget.__init__(self, parent)
+        self.ui = Ui_send_chanmsg_dialog()
+        self.ui.setupUi(self)
+        self.parent = parent
+        QtGui.QWidget.resize(self, QtGui.QWidget.sizeHint(self))
+
+class aboutDialog(QtGui.QDialog):
+    def __init__(self, parent):
+        QtGui.QWidget.__init__(self, parent)
+        self.ui = Ui_about_dialog()
+        self.ui.setupUi(self)
+        self.parent = parent
+        QtGui.QWidget.resize(self, QtGui.QWidget.sizeHint(self))
+
+
+def run():
+    app = QtGui.QApplication(sys.argv)
+    myapp = MyForm()
+    myapp.show()
+    sys.exit(app.exec_())
+    
+    
